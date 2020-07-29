8ff9c7eb9ae757c362eeb7e0b834dfd494fdaec2
navratna98 <67947428+navratna98@users.noreply.github.com>
2020-07-29 17:14:44 +0530

    Add files via upload

---
+++ clickjacking_test.py
@@ -0,0 +1,57 @@
+from urllib.request import urlopen
+from sys import argv,exit
+
+__author__="Navratna Vats"
+
+def checking(url):
+    ' to check if url is vulnerable or not'
+
+    try:
+        if "http" not in url:
+            url="https://"+url
+            data=urlopen(url)
+            headers=data.info()
+            if "X-Frame-Options" not in headers:
+                return true
+    except :
+        return false
+
+
+def proof(url):
+    payload=""
+    <html>
+      <head>
+        <title>Click jacking test page</title>
+      </head>
+      <body>
+        <p>Website is vulerable to clickjacking...</p>
+        <iframe src="{}" width="500" height="500"></iframe>
+
+      </body>
+    </html> "".format(url)
+    with open(url +".html" ,"w") as file:
+        file.write(code)
+        file.close()
+
+def main():
+    try:
+        sites:open(argv[1] , 'r').readlines()
+    except:
+        print("[-] Usage: python(3) clickjacking_test.py <file_name>");exit(0)
+
+        for site in sites[0:]:
+            print("\n[-] Checking " + site)
+            status=checking(site)
+
+            if(status):
+                print("[+] Website is Vulnerable!!")
+                proof(site.split(\n)[0])
+
+                print("[-] Proof Created and saved as <URL>.html")
+            elif not status:
+                print("[-] Website is not Vulerable!")
+
+            else:
+                print("Python Crashed please Re-Launch")
+
+if __name__=='__main__':main()
